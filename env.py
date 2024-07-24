7acad44b615d6f615cebd47126ff699ff9920fb2
CallumNewman <ballumnew@gmail.com>
2024-07-24 21:57:34 +0100

    Added new secret key

--- env.py
+++ env.py
@@ -3,3 +3,7 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uelfpcuogus:dmZhSwGCisPR@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/jeep_debt_carry_999779"
 )
+
+os.environ.setdefault(
+    "SECRET_KEY", "g1eJCiFrPdCHKpvjEaX0RslDI9U5nMi2"
+)
